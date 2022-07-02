95143e74159eaf4e76d2dd2b4b037699b1bdd962
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-02 02:51:35 -0400

    update

--- mm.c
+++ mm.c
@@ -301,7 +301,7 @@ bool mm_checkheap(int lineno)
         succ = ItP(get(succ + 8));
     }
 
-    dbg_printf("Heap is consistent\n");
+    dbg_printf("Heap is consistent at line %d\n", lineno);
 #endif /* DEBUG */
     return true;
 }

78eafc53cbeee2df3bd4d10226aeccf53286b16d
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-02 12:22:03 -0400

    update

--- mm.c
+++ mm.c
@@ -29,7 +29,7 @@
  * uncomment the following line. Be sure not to have debugging enabled
  * in your final submission.
  */
-//  #define DEBUG
+ #define DEBUG
 
 #ifdef DEBUG
 /* When debugging is enabled, the underlying functions get called */

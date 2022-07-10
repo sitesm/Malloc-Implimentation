470fc05b3826d1b678081f9431de3f54c47daf16
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-10 00:33:25 -0400

    Allocate page now takes in a value to increase memory util

--- mm.c
+++ mm.c
@@ -188,19 +188,19 @@ void* malloc(size_t size){
     // tmp_pos = how far the block will extend; also next PP
     void *tmp_pos = TOH + block_size; 
     
-    if(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
-        size_t req_size =  PtI(tmp_pos) - PtI(mem_heap_hi());
-        // allocate_page((size_t)pow(2, ceil(log2(req_size))));
-        allocate_page(align(req_size+128)); // Add overhead
-    }
-
-    // // allocate page if tmp_pos exceeds the current heap size (Minus the epilogue header) 
-    // while(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
-    //     if(!allocate_page(32768)){
-    //         printf("Page allocation failed during malloc");
-    //         return NULL;
-    //     }
+    // if(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
+    //     size_t req_size =  PtI(tmp_pos) - PtI(mem_heap_hi());
+    //     // allocate_page((size_t)pow(2, ceil(log2(req_size))));
+    //     allocate_page(align(req_size+128)); // Add overhead
     // }
+
+    // allocate page if tmp_pos exceeds the current heap size (Minus the epilogue header) 
+    while(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
+        if(!allocate_page(262144)){
+            printf("Page allocation failed during malloc");
+            return NULL;
+        }
+    }
 
     // place the block at the top of the heap
     allocated_size = place((void*)TOH, block_size);

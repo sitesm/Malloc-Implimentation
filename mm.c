3bfce89039bcdfbf7ec3cc37ba62b65f882f0d4d
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-10 01:25:23 -0400

    Going back to try other page size values

--- mm.c
+++ mm.c
@@ -195,7 +195,7 @@ void* malloc(size_t size){
     //     // allocate_page(req_size); // Add overhead
     // }
 
-    allocate page if tmp_pos exceeds the current heap size (Minus the epilogue header) 
+    // allocate page if tmp_pos exceeds the current heap size (Minus the epilogue header) 
     while(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
         if(!allocate_page(32768)){
             printf("Page allocation failed during malloc");

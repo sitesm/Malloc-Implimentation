df30ffe57e7f7ebd2959f2ba9f632237658a9dd6
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-10 03:27:32 -0400

    -- EXPLICIT LIST --: Changed from first fit to best fit

--- mm.c
+++ mm.c
@@ -185,7 +185,7 @@ void* malloc(size_t size){
 
     // allocate page if tmp_pos exceeds the current heap size (Minus the epilogue header) 
     while(tmp_pos > (void*)((char*)mem_heap_hi() - 8)){
-        if(!allocate_page(32768)){
+        if(!allocate_page(16384)){
             printf("Page allocation failed during malloc");
             return NULL;
         }

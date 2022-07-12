9ae7132940d43eb6d06df4b5ed075f47d09e371c
Matthew Sites <64267858+sitesm@users.noreply.github.com>
2022-07-12 16:58:44 -0400

    CHANGE: Trying a power of 2's approach to the segregated block sizes

--- mm.c
+++ mm.c
@@ -1030,33 +1030,62 @@ void* ItP(size_t ptr_int){
 int get_index(size_t block_size){
 
     // Block will never be smaller then 32
-    if(block_size == 32){
+    // if(block_size == 32){
+    //     return 0;
+    // }else if(block_size == 48){
+    //     return 1;
+    // }else if(block_size == 64){
+    //     return 2;
+    // }else if(block_size == 80){
+    //     return 3;
+    // }else if(block_size == 96){
+    //     return 4;
+    // }else if(block_size == 112){
+    //     return 5;
+    // }else if(block_size == 128){
+    //     return 6;
+    // }else if(128 < block_size && block_size < 256){
+    //     return 7;
+    // }else if(256 <= block_size && block_size < 512){
+    //     return 8;
+    // }else if(512 <= block_size && block_size < 1024){
+    //     return 9;
+    // }else if(1024 <= block_size && block_size < 4096){
+    //     return 10;
+    // }else if(4096 <= block_size && block_size < 16384){
+    //     return 11;
+    // }else if(16384 <= block_size && block_size < 66536){
+    //     return 12;
+    // }else{
+    //     return 13;
+    // }
+
+    // Attempt 2
+    if(32 <= block_size < 64){
         return 0;
-    }else if(block_size == 48){
+    }else if(64<= block_size < 128){
         return 1;
-    }else if(block_size == 64){
+    }else if(128<= block_size < 256){
         return 2;
-    }else if(block_size == 80){
+    }else if(256<= block_size < 512){
         return 3;
-    }else if(block_size == 96){
+    }else if(512<= block_size < 1024){
         return 4;
-    }else if(block_size == 112){
+    }else if(1024<= block_size < 2048){
         return 5;
-    }else if(block_size == 128){
+    }else if(2048<= block_size < 4096){
         return 6;
-    }else if(128 < block_size && block_size < 256){
+    }else if(4096<= block_size < 8192){
         return 7;
-    }else if(256 <= block_size && block_size < 512){
+    }else if(8192<= block_size < 16384){
         return 8;
-    }else if(512 <= block_size && block_size < 1024){
+    }else if(16384<= block_size < 32768){
         return 9;
-    }else if(1024 <= block_size && block_size < 4096){
+    }else if(32768 <= block_size < 66536){
         return 10;
-    }else if(4096 <= block_size && block_size < 16384){
+    }else if(65536 <= block_size < 131072){
         return 11;
-    }else if(16384 <= block_size && block_size < 66536){
-        return 12;
     }else{
-        return 13;
+        return 12;
     }
 }
